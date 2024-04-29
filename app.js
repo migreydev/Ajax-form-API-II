62e3240bb4d25c5d5437bb3bfbce203f49276c5d
Miguel Ángel Reyes Méndez <49988347+migreydev@users.noreply.github.com>
2024-04-29 20:41:12 +0200

    Subiendo proyecto

---
+++ app.js
@@ -0,0 +1,9 @@
+const urlIncidencias = "http://localhost:3000/incidencias";
+const urlUsuarios = "http://localhost:3000/usuarios";
+const urlAulas = "http://localhost:3000/aulas";
+
+const fecha = document.getElementById('fecha');
+const email = document.getElementById('email');
+const nombre = document.getElementById('nombre');
+const telefono = document.getElementById('hora');
+const aula = document.getElementById('descripcion');
